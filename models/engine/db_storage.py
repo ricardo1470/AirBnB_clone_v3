d3134abeee8dfacbb8fdd3c99826797b0d7cdfa4
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-09-09 02:11:18 -0500

    update file

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,11 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+    
+    def get(self, cls, id):
+        """ aqui escribire la documentacion """
+        pass
+
+    def count(self, cls=None):
+        """ aqui escribire la documentacion """
+        pass
