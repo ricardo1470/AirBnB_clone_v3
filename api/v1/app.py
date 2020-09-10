95560f1e41560d70afae4e38d77a676454f6dbfd
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-09-10 16:37:30 -0500

    update app

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,8 +8,10 @@ from models import storage
 from api.v1.views import app_views
 from os import getenv
 from flask_cors import CORS
+from flasgger import Swagger
 
 app = Flask(__name__)
+swagger = Swagger(app)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
@@ -29,4 +31,4 @@ def My404(error):
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST', default='0.0.0.0')
     port = getenv('HBNB_API_PORT', default=5000)
-    app.run(host=host, port=int(port), threaded=True)
+    app.run(host=host, port=int(port), threaded=True, debug=True)
